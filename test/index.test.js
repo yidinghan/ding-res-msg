0babc7539af3a805b187c5ee8434ff658befb393
ding <playdingnow@gmail.com>
2017-05-15 13:24:24 +0800

    test(res msg): init test case

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+const { test } = require('ava');
+
+const resMsg = require('../');
+
+
+test('get success msg without any input', (t) => {
+  const { success, data } = resMsg();
+  t.true(success, 'success flag');
+  t.is(data, undefined, 'should be undefined when not giving data');
+});
