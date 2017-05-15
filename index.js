917f7499aa0ce442a6232e27430b28c0492bf2a3
ding <playdingnow@gmail.com>
2017-05-15 14:01:54 +0800

    refactor(res msg): use object input arguments

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 const isProd = /prod/.test(process.env.NODE_ENV);
 
-const resMsg = (error, data, statusCode = 400, isPagination = false) => {
+const parseArguments = (payload = {}) => {
+  if (payload instanceof Error) {
+    return { error: payload };
+  }
+
+  if (payload.error !== undefined && typeof payload.error === 'string') {
+    return Object.assign(payload, {
+      error: new Error(payload.error),
+    });
+  }
+
+  return payload;
+};
+
+/**
+ * res msg formattor
+ *
+ * @param {object} payload - input arguments
+ */
+const resMsg = (payload = {}) => {
+  const {
+    error,
+    data,
+    code = 400,
+    isPagination = false,
+  } = parseArguments(payload);
+
   let msg = { success: true, data };
   if (!error) {
     if (isPagination) {
@@ -9,8 +35,8 @@ const resMsg = (error, data, statusCode = 400, isPagination = false) => {
     return msg;
   }
 
-  const code = error.statusCode || statusCode;
-  msg = { success: false, error, code };
+  const finalCode = error.code || error.statusCode || code;
+  msg = { success: false, error, code: finalCode };
   msg.error = error.message || error;
   if (!isProd) {
     msg.stack = (typeof error.stack === 'string' && error.stack.split('\n')) || error.stack;
